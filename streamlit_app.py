e157beb35278bc7e2ac08627726c70847feb6af7
zordon8000 <71290022+zordon8000@users.noreply.github.com>
2022-08-19 11:16:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit as st
 
 st.title('My parents are New Healthy Diner')
+st.header('Breakfast menu')
+st.text('Omega 3 blueberry Oatmeal')
+st.text('Kale, Spinach & Rokkie smoothie')
+st.text('Hard-Boiled Free-Range Egg')
+
+
