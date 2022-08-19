4acd4f612cf90654aeb15789aebda6d1a0a7b9e6
zordon8000 <71290022+zordon8000@users.noreply.github.com>
2022-08-19 11:41:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
-my_fruit_list.set_index('Fruits', inplace=True)
+my_fruit_list = my_fruit_list.set_index('Fruits')
 
 st.text('Pick some fruits :)')
 
